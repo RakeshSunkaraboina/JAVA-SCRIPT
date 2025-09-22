9a2c6ed0cc79ce0303df018bb7d587e331588058
rakesh <rakeshsunkaraboina08@gmail.com>
2025-09-22 13:56:55 +0530

    dom changes

---
+++ function.js
@@ -0,0 +1,35 @@
+// function-code-a function is a block of code designed to perform a specific task.
+// function add() {
+//     var a = 67;
+//     var b = 78;
+//     document.write(a + b);
+//     console.log(a + b);
+// };
+
+// add();
+// ES6
+
+// const add = () => {
+//      var a = 87;
+//     var b = 88;
+//     document.write(a + b);
+//     console.log(a + b);
+// };
+
+// add();
+
+// ES7
+// const add = (a, b) => console.log(a + b);
+// add(78, 90);
+
+// hosting-to declare the variable to the top
+// console.log(b);
+// b = 45;
+
+// let name = "John";
+// function demo(){
+//     let name = "Rakesh";
+//     document.write(`Hello, my name is ${name}`);
+// }
+// document.write(`Hello, my name is ${name}`);
+// demo();
