9611c81dde7e78d48cd0249b68d1c97e880e8b5f
rakesh <rakeshsunkaraboina08@gmail.com>
2025-09-22 10:45:31 +0530

    changes

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var a = 60;
 var a = 90;
 let b =68;
-let b = 90;
 const c = 78;
 console.log("value of b is $(b)");
+console.log("value of c is $(c)");
 console.log(a);
+
+//data types in java Script
+// primitive and non primitive
+// primitive : Number,string,boolean,Null,undefined
+// Non-primitive: Array,Object
+// typeof()-how the type of data
+// this,window-commands to display the functions of js
+
+a = 7463;
+console.log(typeof a);
+name ="john";
+console.log(typeof name);
